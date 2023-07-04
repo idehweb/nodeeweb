b73595896f30aae019d1fa3359cc5b0b6ffd80a2
Erfan <erfanmahvash12345@gmail.com>
2023-07-04 11:37:51 +0330

    add:color to morgan log

--- core/src/handlers/log.handler.ts
+++ core/src/handlers/log.handler.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import { MiddleWare } from "../../types/global";
 import { createCustomLogger } from "../core/log";
 import store from "../../store";
+import { Colors, color } from "../../utils/color";
 
 export class Logger {
   constructor(private logger: winston.Logger, private label?: string) {}
@@ -44,14 +45,112 @@ export function createLogger(name: string, maxFiles = 1) {
 const morganLogger = createLogger("core.server", 5);
 const morganStream = {
   write: (msg: string) => {
-    return morganLogger.log(msg.replace(/\n$/, ""));
+    const {
+      agent,
+      content_length,
+      ip,
+      method,
+      response_time,
+      status,
+      url,
+    }: MorganDetailType = JSON.parse(msg);
+
+    let final_msg = "";
+
+    // ip , agent
+    if (!store.env.isLoc) {
+      final_msg += `${ip} ${agent}`;
+    }
+
+    // method
+    let method_color: keyof typeof Colors;
+    if (store.env.isLoc) {
+      switch (method.toUpperCase()) {
+        case "GET":
+          method_color = "Green";
+          break;
+        case "POST":
+          method_color = "Yellow";
+          break;
+        case "PUT":
+        case "PATCH":
+          method_color = "Blue";
+          break;
+        case "DELETE":
+          method_color = "Red";
+          break;
+        default:
+          method_color = "White";
+      }
+    } else {
+      method_color = "White";
+    }
+    final_msg += " " + color(method_color, method.toUpperCase());
+
+    // url
+    final_msg += " " + url;
+
+    // code
+    let status_color: keyof typeof Colors;
+    if (store.env.isLoc) {
+      switch (Math.floor(status / 100)) {
+        case 1:
+          status_color = "Cyan";
+          break;
+        case 2:
+          status_color = "Green";
+          break;
+        case 3:
+          status_color = "Blue";
+          break;
+        case 4:
+          status_color = "Red";
+          break;
+        case 5:
+        default:
+          status_color = "Magenta";
+          break;
+      }
+    } else {
+      status_color = "White";
+    }
+    final_msg += " " + color(status_color, String(status));
+
+    // time
+    final_msg += " - " + response_time + "ms";
+
+    // content length
+    if (content_length) final_msg += " - " + content_length;
+
+    return morganLogger.log(final_msg);
   },
 };
 
+type MorganDetailType = {
+  method: string;
+  url: string;
+  status: number;
+  content_length: string;
+  response_time: number;
+  ip: string;
+  agent: string;
+};
+
 export const expressLogger: MiddleWare = morgan(
-  `${
-    store.env.isLoc ? "" : ":remote-addr "
-  }:method :url :status :res[content-length] - :response-time ms`,
+  // `${
+  //   store.env.isLoc ? "" : ":remote-addr "
+  // }:method :url :status :res[content-length] - :response-time ms`,
+  (tokens, req, res) => {
+    return JSON.stringify({
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      status: Number.parseInt(tokens.status(req, res)),
+      content_length: tokens.res(req, res, "Content-Length"),
+      response_time: Number.parseFloat(tokens["response-time"](req, res)),
+      ip: tokens["remote-addr"](req, res),
+      agent: tokens["user-agent"](req, res),
+    } as MorganDetailType);
+  },
   {
     stream: morganStream,
   }
