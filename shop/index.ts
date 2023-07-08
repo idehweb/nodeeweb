f5ed074ab068ab5e13ec559d8bddc2f0322bd5c2
Erfan <erfanmahvash12345@gmail.com>
2023-07-08 17:22:06 +0330

    add: shop attribute

--- shop/index.ts
+++ shop/index.ts
@@ -1,7 +1,14 @@
 import deployCore from "@nodeeweb/core/deploy";
+import { createLogger } from "@nodeeweb/core/src/handlers/log.handler";
+import store from "@nodeeweb/core/store";
+import registerAttribute from "./src/attributes";
 
 async function deployShop() {
   await deployCore();
+  store.systemLogger = createLogger("shop", "Shop", 5);
+
+  // register entity
+  registerAttribute();
 }
 
 deployShop();
--- shop/src/attributes/model.mjs
+++
@@ -1,5 +0,0 @@
-console.log("#model attributes");
-export default (mongoose) => {
-  const AttributesSchema = new mongoose.Schema();
-  return AttributesSchema;
-};
