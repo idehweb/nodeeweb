0e82c3f05dd66afebfba73a6db115447085202e4
Erfan <erfanmahvash12345@gmail.com>
2023-07-11 16:01:59 +0330

    remove shop admin assets

--- shop/admin/site_setting/config.js
+++
@@ -1,5 +0,0 @@
-window.BASE_URL = "http://localhost:2759";
-window.ADMIN_URL = "http://localhost:2759";
-window.THEME_URL = "http://localhost:2759/theme";
-window.ADMIN_ROUTE = "http://localhost:2759/admin";
-window.SHOP_URL = "http://localhost:2759/";
