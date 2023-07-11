5e8f94c0047aeb6bb704234eee0c5d0cd38a800a
Erfan <erfanmahvash12345@gmail.com>
2023-07-11 13:49:23 +0330

    remove: admin assets

--- core/admin/site_setting/config.js
+++
@@ -1,5 +0,0 @@
-window.BASE_URL = "http://localhost:2758";
-window.ADMIN_URL = "http://localhost:2758";
-window.THEME_URL = "http://localhost:2758/theme";
-window.ADMIN_ROUTE = "http://localhost:2758/admin";
-window.SHOP_URL = "http://localhost:2758/";
