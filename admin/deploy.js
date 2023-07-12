fea2b3bc06782d1a863c124e0211ffe91255e9f9
Idehweb <34858656+idehweb@users.noreply.github.com>
2023-07-12 13:58:07 +0330

    Admin dev tools (#6)
    * add some dev tools
    
    * update eslint configs
    
    * yarn upgrade react-scripts
    
    * add bundle-analyze configs to craco
    
    * add REACT_APP_VERSION on production
    
    * add dev, analyze script
    
    * Update deploy.js
    
    * update prettier configs
    
    * Update .gitignore
    
    * Delete ComponentOld.js
    
    * update .gitignore
    
    * Update tsconfig.json
    
    * Delete updateBack.sh
    
    * Create .env
    
    * add bundle optimization configs
    
    * remove unused packages
    
    * remove unused lodash usages
    
    ---------
    
    Co-authored-by: Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>

--- admin/deploy.js
+++ admin/deploy.js
@@ -1,15 +1,19 @@
-var FtpDeploy = require('ftp-deploy');
-var ftpDeploy = new FtpDeploy();
+const FtpDeploy = require('ftp-deploy');
+
+const ftpDeploy = new FtpDeploy();
+
 require('dotenv').config({ path: '.env.local' });
 
-var config = {
+const config = {
+  sftp: process.env.FTP_SFTP,
   user: process.env.FTP_USER,
   // Password optional, prompted if none given
   password: process.env.FTP_PASS,
   host: process.env.FTP_HOST,
   port: process.env.FTP_PORT || 21,
-  localRoot: __dirname + '/../admin',
+  localRoot: __dirname + '/build',
   remoteRoot: process.env.FTP_REMOTE_ROOT,
+
   // this would upload everything
   include: ['*'],
   // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
@@ -17,12 +21,13 @@ var config = {
   // delete ALL existing files at destination before uploading, if true
   deleteRemote: process.env.FTP_DELETE,
   // Passive mode is forced (EPSV command is not sent)
-  forcePasv: true
+  forcePasv: true,
+  debugMode: false,
 };
 
 ftpDeploy
   .deploy(config)
-  .then(() => console.log('finished ;)'))
+  .then((res) => console.log('finished:', res))
   .catch((err) => console.log(err));
 
 ftpDeploy.on('uploading', (d) => {
--- admin/updateBack.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#cp -r ./build/ ../nodeeweb-server/theme &&
-rm -rf ../nodeeweb-server/admin/* && cp -r ./build/ ../nodeeweb-server/admin
