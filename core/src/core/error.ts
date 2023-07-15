253e5d784a98a3794299ed2991a05306361f7d7c
Erfan <erfanmahvash12345@gmail.com>
2023-07-15 17:58:11 +0330

    change: global error handler

--- core/src/core/error.ts
+++ core/src/core/error.ts
@@ -1,6 +1,7 @@
-import store from "../../store";
-import { ErrorType } from "../../types/error";
-import { MiddleWare, MiddleWareError } from "../../types/global";
+import store from '../../store';
+import { ErrorType } from '../../types/error';
+import { MiddleWare, MiddleWareError } from '../../types/global';
+import logger from '../handlers/log.handler';
 
 export class GeneralError extends Error {
   isGeneral = true;
@@ -55,14 +56,25 @@ export class LimitError extends GeneralError {
 }
 
 export const errorHandler: MiddleWareError = (error, req, res, next) => {
-  console.log("#Error", error);
+  let code = 500,
+    message = error.message,
+    err = error,
+    extraProps: any = {};
+  if (Math.floor(code / 100) === 5) logger.error(error);
+  if (error instanceof GeneralError) {
+    code = error.code;
+    extraProps.type = error.type;
+    message = error.message;
+    err = error;
+  }
+
   return res
-    .status(500)
-    .json({ status: "error", message: error.message, error });
+    .status(code)
+    .json({ status: 'error', ...extraProps, message, error: err });
 };
 
 export const notFoundHandler: MiddleWare = (req, res, next) => {
-  return res.status(404).json({ status: "error", message: "path not found" });
+  return res.status(404).json({ status: 'error', message: 'path not found' });
 };
 
 export function setErrorPackage() {
