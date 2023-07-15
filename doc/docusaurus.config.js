6e09e174a8ac5815fb8c91f66f9dc9efe0eae4fe
Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>
2023-07-15 11:14:48 +0330

    Update docusaurus.config.js

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Nodeeweb Document',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Nodeeweb',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -132,6 +132,11 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
       },
     }),
 };
