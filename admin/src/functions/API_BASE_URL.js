b46f215c4d59468821ff8a97cd87bf3484a58955
Mohammadreza <mrhhajiyan@gmail.com>
2023-07-26 22:59:01 +0330

    fixed api functions and auth provider

--- admin/src/functions/API_BASE_URL.js
+++ admin/src/functions/API_BASE_URL.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-export const BASE_URL = `${
+export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? process.env.REACT_APP_API_BASE_URL_DEV
-    : window.origin
-}/api/v1`;
-export const ADMIN_ROUTE = window.ADMIN_ROUTE;
-export const ShopURL = window.SHOP_URL;
+    : window.origin;
+export const ADMIN_ROUTE = BASE_URL + '/admin';
+export const ShopURL = BASE_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
