e65964d7c843b183c75061b776edb8d22e8ecc51
Erfan <erfanmahvash12345@gmail.com>
2023-08-02 11:34:36 +0330

    detect mongoose validation error

--- core/utils/error.ts
+++ core/utils/error.ts
@@ -1,12 +1,21 @@
-import { DuplicateError, ErrorType, GeneralError } from '../types/error';
+import {
+  DuplicateError,
+  ErrorType,
+  GeneralError,
+  ValidationError,
+} from '../types/error';
 
 export function errorDetector(err: any): GeneralError {
   if (err instanceof GeneralError) return err;
 
-  const { code, name, message, stack } = err ?? {};
+  const { code, name, message = '', stack } = err ?? {};
   if (name === 'MongoServerError' || name === 'MongooseError') {
     if (code === 11000) return new DuplicateError(message, stack);
     return new GeneralError(message, 500, ErrorType.DB, undefined, stack);
   }
+
+  if ((message ?? '').startsWith('ValidationError'))
+    return new ValidationError(message, undefined, stack);
+
   return new GeneralError(message, 500, ErrorType.Unknown, undefined, stack);
 }
