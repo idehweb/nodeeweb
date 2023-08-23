eb4f0d6aa63c7992171d703bead6c2f85a018a29
Idehweb <34858656+idehweb@users.noreply.github.com>
2023-08-23 17:46:10 +0330

    fixe: read data from server (#36)
    * fixe: read data from server
    
    * update
    
    * bump version
    
    ---------
    
    Co-authored-by: Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import _ from 'underscore';
+import _isEqual from 'lodash/isEqual'
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
@@ -6,16 +6,17 @@ import store from '#c/functions/store';
 import { SaveData } from '#c/functions/index';
 import { useSelector, useDispatch } from 'react-redux';
 import createRoutes from '#c/DefaultRoute';
+
 const APP = (props) => {
   let { t } = props,
     routes = [];
-  const themeData = useSelector((st) => st.store.themeData, _.isEqual);
+  const themeData = useSelector((st) => st.store.themeData, _isEqual);
   if (themeData && themeData.routes) {
     routes = createRoutes(themeData.routes);
   }
-  if (!themeData || (themeData && !themeData.models)) {
-    return <></>;
-  }
+  // if (!themeData || (themeData && !themeData.models)) {
+  //   return <></>;
+  // }
   return (
     <div className={t('languageDir')} dir={t('languageDir')}>
       <BrowserRouter>
--- front/src/views/Page.js
+++ front/src/views/Page.js
@@ -1,61 +1,33 @@
-import React, {useEffect, useState} from "react";
-import {Container} from "shards-react";
-import {useNavigate, useParams} from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { Container } from 'shards-react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import {withTranslation} from "react-i18next";
-import {dFormat, PriceFormat} from "#c/functions/utils";
-import MainContent from '#c/components/MainContent';
+import MainContent from '@/components/MainContent';
+import { getPage, isClient, loadPost } from '@/functions';
 
-import {
-  addBookmark,
-  clearPost,
-  getPage,
-  isClient,
-  loadPost,
-  loveIt,
-  MainUrl,
-  savePost,
-  setStyles
-} from "#c/functions/index";
-import {SnapChatIcon} from "#c/assets/index";
+import Loading from '@/components/Loading';
+import store from '@/functions/store';
 
-import Loading from "#c/components/Loading";
-import store from "../functions/store";
-import {useSelector} from "react-redux";
-// import { Link, useNavigate, useParams } from "react-router-dom";
-// let obj = ;
-// let the_id='';
-
-const Page = (props) => {
-  // console.clear()
-  let {match, location, history, t, url} = props;
-
-  let page = useSelector((st) => {
+export default function Page(props) {
+  const page = useSelector((st) => {
     return st.store.page || [];
   });
-  let {firstName, lastName, internationalCode, token} = store.getState().store.user;
-  let navigate=useNavigate();
+  const { firstName, lastName, internationalCode, token } =
+    store.getState().store.user;
+  const navigate = useNavigate();
 
-  // window.scrollTo(0, 0);
-  let params = useParams();
-  let the_id = params._id;
-  // let search = false;
-  // let history = useNavigate();
+  const params = useParams();
+  const the_id = params._id;
 
-
-  const [mainId, setMainId] = useState(the_id);
-  const [tab, setTab] = useState("description");
-  const [state, setState] = useState(isClient ? [] : (page || []));
-  const [lan, setLan] = useState(store.getState().store.lan || "fa");
-
+  const [state, setState] = useState(isClient ? [] : page || []);
 
   const getThePost = (_id) => {
     return new Promise(function (resolve, reject) {
-
       getPage(_id).then((d = {}) => {
-        if(d.success==false && (d.access && d.access=='private')){
-          let redirect_url = "/login/";
-          navigate(redirect_url)
+        if (d.success == false && d.access && d.access == 'private') {
+          let redirect_url = '/login/';
+          navigate(redirect_url);
           // }
         }
         if (d._id) {
@@ -83,36 +55,32 @@ const Page = (props) => {
             elements: d.elements,
             thumbnail: d.thumbnail,
             excerpt: d.excerpt,
-            views: d.views
+            views: d.views,
           });
         } else {
           reject({
             load: true,
-            notfound: true
+            notfound: true,
           });
         }
       });
     });
   };
-  if (isClient)
-    useEffect(() => {
-      // let mounted = true;
-      let {_id, title} = params;
-      // console.clear()
-      // if (_id != the_id)
-      getThePost(the_id)
-        .then(items => {
-          setState(items);
-          if (isClient)
-            window.scrollTo(0, 0);
-          // }
-        }).catch(e => {
+
+  useEffect(() => {
+    if (!isClient) return;
+
+    getThePost(the_id)
+      .then((items) => {
+        setState(items);
+        if (isClient) window.scrollTo(0, 0);
+        // }
+      })
+      .catch((e) => {
         setState(e);
-
-
       });
-      // return () => mounted = false;
-    }, [the_id]);
+    // return () => mounted = false;
+  }, [the_id]);
 
   let {
     load,
@@ -129,27 +97,30 @@ const Page = (props) => {
     backgroundColor,
     notfound,
     enableAdmin = false,
-    views = null, elements = null
+    views = null,
+    elements = null,
   } = state;
-  if (redirect && isClient) return <Navigate to={redirect}/>;
-  if (!load && isClient) return <Loading/>;
+  if (redirect && isClient) return <Navigate to={redirect} />;
+  if (!load && isClient) return <Loading />;
   if (load && notfound && isClient) return <div>not found</div>;
-  let style = setStyles({backgroundColor});
+
   return (
-
-    <Container className={"main-content-container p-0 pb-4 kiuytyuioiu bg-white " + classes} key={0}>
-
-      {/*<div style={style} className={'the-body ghui'} key={1}>*/}
-      <MainContent elements={elements} kind={kind} maxWidth={maxWidth} backgroundColor={backgroundColor}/>
-      {/*</div>*/}
-
+    <Container
+      className={
+        'main-content-container p-0 pb-4 kiuytyuioiu bg-white ' + classes
+      }>
+      <MainContent
+        elements={elements}
+        kind={kind}
+        maxWidth={maxWidth}
+        backgroundColor={backgroundColor}
+      />
     </Container>
   );
-};
+}
 export const PageServer = [
   {
     func: loadPost,
-    params: "6217502008d0e437d6b4ad97"
-  }
+    params: '6217502008d0e437d6b4ad97',
+  },
 ];
-export default withTranslation()(Page);
