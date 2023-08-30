bc1e0df76553b672acccaa6bcade3fe60a028361
Erfan <erfanmahvash12345@gmail.com>
2023-08-30 12:16:00 +0330

    rm: sms test endpoint

--- shop/src/gateways/controller.ts
+++ shop/src/gateways/controller.ts
@@ -1,42 +1,9 @@
-import {
-  CRUD_DEFAULT_REQ_KEY,
-  PUBLIC_ACCESS,
-} from '@nodeeweb/core/src/constants/String';
+import { PUBLIC_ACCESS } from '@nodeeweb/core/src/constants/String';
 import { ControllerAccess } from '@nodeeweb/core/types/controller';
 import { registerEntityCRUD } from '@nodeeweb/core/src/handlers/entity.handler';
-import { controllerRegister } from '@nodeeweb/core/src/handlers/controller.handler';
-import Service from './service';
-import store from '../../store';
-import {
-  CorePluginType,
-  SMSPluginContent,
-  SMSPluginType,
-} from '@nodeeweb/core/types/plugin';
 
 export default function registerController() {
   const access: ControllerAccess = { modelName: 'admin', role: PUBLIC_ACCESS };
-
-  controllerRegister(
-    {
-      method: 'get',
-      service: async (req, res) => {
-        const phone = req.query.phone;
-        const plugin = store.plugins.get(
-          CorePluginType.SMS
-        ) as SMSPluginContent;
-        const sendSMS = plugin.stack[0];
-        const response = await sendSMS({
-          to: phone as string,
-          type: SMSPluginType.Manual,
-          text: 'hello world!',
-        });
-        return res.json(response);
-      },
-      url: '/sms',
-    },
-    { base_url: '/api/v1/gateway' }
-  );
-
   // crud
   registerEntityCRUD(
     'gateway',
