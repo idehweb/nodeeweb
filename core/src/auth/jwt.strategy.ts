d1e720e6f83ea7e90e15ae930aba36a3c9858708
Erfan <erfanmahvash12345@gmail.com>
2023-09-03 16:16:46 +0330

    rm: useless import

--- core/src/auth/jwt.strategy.ts
+++ core/src/auth/jwt.strategy.ts
@@ -1,41 +1,11 @@
 import { NextFunction } from 'express';
 import { AuthStrategy } from '../../types/auth';
 import { Req, Res } from '../../types/global';
-import store from '../../store';
-import {
-  ErrorType,
-  ForbiddenError,
-  GeneralError,
-  NotFound,
-  NotImplement,
-  SendSMSError,
-  UnauthorizedError,
-} from '../../types/error';
-import {
-  CorePluginType,
-  SMSPluginContent,
-  SMSPluginType,
-  SmsSendStatus,
-} from '../../types/plugin';
-import randomNumber from 'random-number-csprng';
-import {
-  authWithToken,
-  extractToken,
-  setToCookie,
-  signToken,
-  verifyToken,
-} from '../handlers/auth.handler';
-import { Document, Model } from 'mongoose';
-import { CoreValidationPipe } from '../core/validate';
+import { authWithToken } from '../handlers/auth.handler';
 
 export const JWT_STRATEGY = 'jwt';
 export class JwtStrategy extends AuthStrategy {
   strategyId = JWT_STRATEGY;
-
-  private get validation() {
-    return (store.globalMiddleware.pipes['validation'] ??
-      new CoreValidationPipe()) as CoreValidationPipe;
-  }
 
   async detect(req: Req, res: Res, next: NextFunction) {
     return this.login(req, res);
