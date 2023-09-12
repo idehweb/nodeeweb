4de73f89931519a3174f90e6c1689009b49a1699
Erfan <erfanmahvash12345@gmail.com>
2023-09-12 15:48:43 +0330

    fix: compose bug

--- shop/compose.sh
+++ shop/compose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ./compose.sh dev -d
 set -aux
-args=( "$@" )
+args=($@)
 git pull
 cd ..
 docker compose -f shop/${1:-dev}.compose.yml down --rmi all
-docker compose -f shop/${1:-dev}.compose.yml up ${args:1}
+docker compose -f shop/${1:-dev}.compose.yml up ${args[@]:1}
