cc301aed93aa152ce25246e83cb67340137a3be7
Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>
2023-09-30 13:06:27 +0330

    Update craco.config.js

--- front/craco.config.js
+++ front/craco.config.js
@@ -1,51 +1,103 @@
+// @ts-check
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
 
 const gitHash = require('child_process')
   .execSync('git rev-parse HEAD')
   .toString()
   .trim();
 
-const now = new Date().toLocaleString('en-us');
+const now = new Date().toISOString();
 process.env.REACT_APP_VERSION = now + ', ' + gitHash;
 
-module.exports = {
+const isDev = process.env.NODE_ENV === 'development';
+const JsBasePath = 'static/js';
+
+/** @type { import("@craco/types").CracoConfig } */
+const config = {
+  eslint: {
+    enable: isDev,
+  },
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
       '#c': path.resolve(__dirname, 'src/'),
     },
-    configure: (webpackConfig, { env, paths }) => {
+    configure: (webpackConfig, { env }) => {
       if (env === 'development') return webpackConfig;
+
       webpackConfig.output = {
         ...webpackConfig.output,
-        filename: 'static/js/bundle.js',
-        chunkFilename: 'static/js/[name].chunk.js',
+        chunkFilename: `${JsBasePath}/[name].bundle.js?h=[chunkhash]`,
       };
-      // webpackConfig.optimization = {
-      //   ...webpackConfig.optimization,
-      //
-      //   // runtimeChunk:'single',
-      //   splitChunks: {
-      //     name: true,
-      //   },
-      // };
 
+      webpackConfig.optimization = {
+        ...webpackConfig.optimization,
+        runtimeChunk: 'single',
+
+        splitChunks: {
+          chunks: 'all',
+          minSize: 500000,
+
+          cacheGroups: {
+            defaultVendors: {
+              filename: `${JsBasePath}/bundle.js?h=[chunkhash]`,
+              priority: -100,
+            },
+            'material-icons': {
+              test: /[\\/]node_modules[\\/]@mui[\\/]icons-material[\\/]/,
+              filename: `${JsBasePath}/material-icons.js?h=[chunkhash]`,
+              priority: -10,
+            },
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              filename: `${JsBasePath}/vendors.js?h=[chunkhash]`,
+              priority: -50,
+            },
+            json: {
+              test: /\.json$/i,
+              filename: `${JsBasePath}/data.json`,
+              priority: -20,
+            },
+          },
+        },
+      };
+
+      // @ts-ignore
       webpackConfig.plugins[5] = new MiniCssExtractPlugin({
         // Options similar to the same options in webpackOptions.output
         // both options are optional
-        filename: 'static/css/bundle.css',
-        chunkFilename: 'static/css/[name].chunk.css',
+        filename: 'static/css/bundle.css?h=[chunkhash]',
+        chunkFilename: 'static/css/[name].chunk.css?h=[chunkhash]',
       });
-      webpackConfig.plugins.push(
-        new webpack.optimize.LimitChunkCountPlugin({
-          maxChunks: 1,
-        }),
-      );
+      // webpackConfig.plugins.push(
+      //   new webpack.optimize.LimitChunkCountPlugin({
+      //     maxChunks: 1,
+      //   })
+      // );
+      if (process.env.ANALYZE) {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+        const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
 
-      // console.log('akbar => ', webpackConfig.optimization);
+        // @ts-ignore
+        webpackConfig.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'server',
+            openAnalyzer: true,
+          }),
+          // @ts-ignore
+          new DuplicatePackageCheckerPlugin()
+        );
+      }
+
       return webpackConfig;
     },
   },
+  babel: {
+    plugins: isDev
+      ? []
+      : [['transform-remove-console', { exclude: ['error', 'warn'] }]],
+  },
 };
+
+module.exports = config;
