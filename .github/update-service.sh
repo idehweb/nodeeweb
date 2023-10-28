b7b7e438d57633a9fdb983882ab3accd79f88783
Erfan <erfanmahvash12345@gmail.com>
2023-10-28 15:28:18 +0330

    increase timeout

--- .github/update-service.sh
+++ .github/update-service.sh
@@ -4,7 +4,7 @@ set -e
 # 1: tag
 # 2: service name
 service_arr=($2)
-for service in $service_arr
+for service in ${service_arr[@]}
 do
     echo try to update $service
     sudo docker service update -d --image $1 $service
