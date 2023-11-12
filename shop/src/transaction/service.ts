f4f2e837faa33134a06369e2b81302c54dc1459d
Erfan <erfanmahvash12345@gmail.com>
2023-11-12 11:29:57 +0330

    implement parse filters

--- shop/src/transaction/service.ts
+++ shop/src/transaction/service.ts
@@ -1,14 +1,22 @@
 import { Req } from '@nodeeweb/core';
+import { FilterQuery } from 'mongoose';
+import { ITransaction } from '../../schema/transaction.schema';
 
 class Service {
+  private parseFilter(req: Req): FilterQuery<ITransaction> {
+    const userType = req.user.type;
+    if (userType === 'admin') return { active: true };
+    return { active: true, consumer: req.user._id };
+  }
+
   parseCountFilter(req: Req) {
-    return {};
+    return this.parseFilter(req);
   }
   parseGetAllFilter(req: Req) {
-    return {};
+    return this.parseFilter(req);
   }
   parseGetOneFilter(req: Req) {
-    return {};
+    return { _id: req.params.id, ...this.parseFilter(req) };
   }
   parseCreateBody(req: Req) {
     return req.body;
