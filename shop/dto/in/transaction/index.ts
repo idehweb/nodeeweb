e93a681643311adfaeb50473179f8b992df29ead
Erfan <erfanmahvash12345@gmail.com>
2023-11-12 11:36:37 +0330

    add transaction crud dto

--- shop/dto/in/transaction/index.ts
+++ shop/dto/in/transaction/index.ts
@@ -1,2 +1,107 @@
-export class TransactionCreateBody {}
-export class TransactionUpdateBody {}
+import { IsMongoID, ToMongoID } from '@nodeeweb/core/utils/validation';
+import { Expose } from 'class-transformer';
+import {
+  IsDate,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Types } from 'mongoose';
+import { TransactionStatus } from '../../../schema/transaction.schema';
+
+export class TransactionCreateBody {
+  @Expose()
+  @IsOptional()
+  @ToMongoID()
+  @IsMongoID()
+  consumer?: Types.ObjectId;
+
+  @Expose()
+  @IsOptional()
+  @ToMongoID()
+  @IsMongoID()
+  order?: Types.ObjectId;
+
+  @Expose()
+  @IsNumber()
+  amount: number;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  currency?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  provider?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  authority?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  payment_link?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsEnum(TransactionStatus)
+  status?: TransactionStatus;
+
+  @Expose()
+  @IsOptional()
+  @IsDate()
+  expiredAt?: Date;
+}
+export class TransactionUpdateBody {
+  @Expose()
+  @IsOptional()
+  @ToMongoID()
+  @IsMongoID()
+  consumer?: Types.ObjectId;
+
+  @Expose()
+  @IsOptional()
+  @ToMongoID()
+  @IsMongoID()
+  order?: Types.ObjectId;
+
+  @Expose()
+  @IsOptional()
+  @IsNumber()
+  amount?: number;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  currency?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  provider?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  authority?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsString()
+  payment_link?: string;
+
+  @Expose()
+  @IsOptional()
+  @IsEnum(TransactionStatus)
+  status?: TransactionStatus;
+
+  @Expose()
+  @IsOptional()
+  @IsDate()
+  expiredAt?: Date;
+}
