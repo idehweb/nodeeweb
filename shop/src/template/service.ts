650a3ccf87654d29e284c6c8d5dd3754576e1910
Erfan <erfanmahvash12345@gmail.com>
2023-11-15 12:16:30 +0330

    call template crud event

--- shop/src/template/service.ts
+++ shop/src/template/service.ts
@@ -1,8 +1,9 @@
-import { MiddleWare, NotFound } from '@nodeeweb/core';
+import { CRUD, MiddleWare, NotFound } from '@nodeeweb/core';
 import { updateTemplate } from './view';
 import store from '../../store';
 import { TemplateDocument, TemplateModel } from '@nodeeweb/core/types/template';
 import { Query } from 'mongoose';
+import { getEntityEventName } from '@nodeeweb/core/src/handlers/entity.handler';
 
 class Service {
   get templateModel(): TemplateModel {
@@ -11,6 +12,15 @@ class Service {
   afterCreate: MiddleWare = (req, res) => {
     const template: TemplateDocument = req.crud;
     updateTemplate(template, { action: 'create' });
+
+    // emit event
+    store.event.emit(
+      getEntityEventName('template', { post: true, type: CRUD.CREATE }),
+      template,
+      { type: CRUD.CREATE, model: 'template' },
+      req
+    );
+
     return res.status(201).json({ data: template });
   };
   afterUpdate: MiddleWare = async (req, res) => {
@@ -21,11 +31,29 @@ class Service {
     if (!newT) throw new NotFound('template not found');
 
     updateTemplate(newT, { action: 'update', oldTemplate: oldT });
+
+    // emit event
+    store.event.emit(
+      getEntityEventName('template', { post: true, type: CRUD.UPDATE_ONE }),
+      newT,
+      { type: CRUD.UPDATE_ONE, model: 'template' },
+      req
+    );
+
     return res.status(200).json({ data: newT });
   };
   afterDelete: MiddleWare = async (req, res) => {
     const template = req.crud;
     updateTemplate(template, { action: 'delete' });
+
+    // emit event
+    store.event.emit(
+      getEntityEventName('template', { post: true, type: CRUD.DELETE_ONE }),
+      template,
+      { type: CRUD.DELETE_ONE, model: 'template' },
+      req
+    );
+
     return res.status(204).send();
   };
 }
