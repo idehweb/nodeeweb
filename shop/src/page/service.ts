2c10ee26914943685075a9e79bb26d13af6ed809
Erfan <erfanmahvash12345@gmail.com>
2023-11-20 17:29:51 +0330

    update index html base on page attrs

--- shop/src/page/service.ts
+++ shop/src/page/service.ts
@@ -8,8 +8,10 @@ import {
 } from '@nodeeweb/core/src/handlers/view.handler';
 import { PageDocument } from '../../schema/page.schema';
 import { page2Route } from '@nodeeweb/core/utils/helpers';
-import { Query } from 'mongoose';
+import { FilterQuery, Query } from 'mongoose';
 import { getEntityEventName } from '@nodeeweb/core/src/handlers/entity.handler';
+import { PublishStatus } from '../../schema/_base.schema';
+import { NotFound } from '@nodeeweb/core';
 
 export default class Service {
   static createAfter: MiddleWare = async (req, res) => {
@@ -36,8 +38,12 @@ export default class Service {
   static updateAfter: MiddleWare = async (req, res) => {
     const pageQuery: Query<PageDocument, PageDocument> =
       req[CRUD_DEFAULT_REQ_KEY];
+
     pageQuery.setOptions({ ...pageQuery.getOptions(), new: false });
-    const oldPage = await pageQuery;
+    const oldPage = await pageQuery.exec();
+
+    if (!oldPage) throw new NotFound('page not found');
+
     const newPage = { ...oldPage.toObject(), ...req.body };
 
     if (oldPage.slug !== newPage.slug || oldPage.path !== newPage.path) {
@@ -72,21 +78,17 @@ export default class Service {
 
     return obj;
   }
-  static getOneAfter: MiddleWare = (req, res) => {
-    const menu = req[CRUD_DEFAULT_REQ_KEY];
-    if (menu.access && menu.access == 'private') {
-      if (!req.user) {
-        return res.status(403).json({
-          success: false,
-          _id: menu && menu._id ? menu._id : null,
-          slug: menu && menu.slug ? menu.slug : null,
-          access: 'private',
-          message: 'login please',
-        });
-      }
-    }
-    return res.json({ data: menu });
-  };
+  static getOneFilter(req: Req) {
+    const isAdmin = req.modelName === 'admin';
+    const { slug, id } = req.params;
+    const filter: FilterQuery<PageDocument> = {};
+
+    if (slug) filter.slug = slug;
+    if (id) filter._id = id;
+    if (!isAdmin) filter.status = PublishStatus.Published;
+
+    return filter;
+  }
 
   static deleteAfter: MiddleWare = (req, res) => {
     const page = req.crud;
