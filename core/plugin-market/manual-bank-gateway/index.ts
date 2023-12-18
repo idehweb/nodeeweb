ed60a12ed51c8380a7809b8161060948cad6eabb
Erfan Mahvash <62294376+erfan-272758@users.noreply.github.com>
2023-12-18 13:38:58 +0330

    add manual bank gateway (#108)

---
+++ core/plugin-market/manual-bank-gateway/index.ts
@@ -0,0 +1,54 @@
+import {
+  BankGatewayPluginContent,
+  BankGatewayUnverified,
+  PaymentVerifyStatus,
+} from './type';
+
+let config: {
+  message: string;
+  lifeTime?: number;
+  resolve: (key: string) => any;
+};
+
+function errorLog(from: string, err: any) {
+  const logger = config.resolve('logger');
+  const parser = config.resolve('axiosError2String');
+  logger.error(`${from} error:`, parser(err));
+}
+
+const create: BankGatewayPluginContent['stack'][0] = async () => {
+  return {
+    isOk: true,
+    expiredAt: new Date(
+      Date.now() + (config.lifeTime ?? 7) * 24 * 60 * 60 * 1000
+    ),
+    authority: new Date().toISOString(),
+    payment_link: config.message,
+    message: config.message,
+  };
+};
+
+const verify: BankGatewayPluginContent['stack'][1] = async () => {
+  const error = new Error('manual payment does not have verify');
+  errorLog('verify', error);
+  return {
+    status: PaymentVerifyStatus.Failed,
+    message: error.message,
+  };
+};
+
+const unverified: BankGatewayUnverified = async () => {
+  return [];
+};
+
+function add(arg: any): BankGatewayPluginContent['stack'] {
+  config = arg;
+  return [create, verify, unverified];
+}
+
+function edit(arg: any): BankGatewayPluginContent['stack'] {
+  config = arg;
+  return [create, verify, unverified];
+}
+
+export { add as config, add as active, edit };
