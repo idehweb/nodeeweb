7b78d0bc726b73fcf63bebc30e4c8362a2c89a9c
Erfan <erfanmahvash12345@gmail.com>
2023-12-20 11:47:07 +0330

    update config

--- admin/publish.sh
+++ admin/publish.sh
@@ -7,7 +7,7 @@ cp package.build.json build/package.json
 cd build
 
 # set env
-npm config set _authToken=$NODE_AUTH_TOKEN
 npm config set registry https://registry.npmjs.org/
+npm config set "//registry.npmjs.org/:_authToken"=$NODE_AUTH_TOKEN
 
 npm publish --access public
