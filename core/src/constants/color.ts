80f0ad412d969da92d454b0325b5fa60ce5e2d88
Erfan <erfanmahvash12345@gmail.com>
2024-01-09 16:12:45 +0330

    refactor: constant color ref

---
+++ core/src/constants/color.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_COLOR_PRIMARY = '#2D3488';
+export const DEFAULT_COLOR_SECONDARY = '#fff';
+export const DEFAULT_COLOR_BACKGROUND = '#fff';
+export const DEFAULT_COLOR_FOOTER_BACKGROUND = '#fff';
+export const DEFAULT_COLOR_TEXT = '#000';
