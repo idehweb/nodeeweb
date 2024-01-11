4a783ad331459590bf133c89375d7799a623090a
Erfan <erfanmahvash12345@gmail.com>
2024-01-11 12:00:38 +0330

    rm: logs on core publish

--- core/publish.sh
+++ core/publish.sh
@@ -2,15 +2,10 @@
 set -e
 
 cd dist
-echo "cd to dist"
 
 # set env
 npm config set registry https://registry.npmjs.org/
 npm config set "//registry.npmjs.org/:_authToken"=$NODE_AUTH_TOKEN
 
-echo "set envs"
 
 npm publish --access public
-
-echo "publish done"
-exit 0
