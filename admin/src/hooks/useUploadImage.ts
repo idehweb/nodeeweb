6194e5abdefa8a259ae6825247e93d7306a1e9a3
Arash Salehkhah <southernwavesofmadness@protonmail.com>
2024-01-11 12:50:10 +0330

    Add useUploadImage hook for image uploading

---
+++ admin/src/hooks/useUploadImage.ts
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import axios, { AxiosError } from 'axios';
+
+import { BASE_URL } from '@/functions/API';
+
+/**
+ * Custom hook for uploading an image.
+ * @returns An object containing the loading state, progress percentage, error object, uploadImage function, and file data.
+ */
+const useUploadImage = () => {
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState<AxiosError>();
+  const [fileData, setFileData] = useState<{
+    url: string;
+    _id: string;
+  }>();
+
+  const uploadMedia = async (
+    file: File,
+    onUploadProgress: (percent: number, canceler: any) => void
+  ) => {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('type', file.type);
+
+    const headers = {
+      Authorization: 'Bearer ' + (localStorage.getItem('token') || ''),
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      response: 'json',
+    };
+
+    let cancel;
+    const config = {
+      cancelToken: new axios.CancelToken((c) => {
+        cancel = c;
+      }),
+      onUploadProgress: (ev) => {
+        const percent = (ev.loaded / ev.total) * 100;
+        onUploadProgress(percent, cancel);
+      },
+      headers,
+    };
+
+    try {
+      const res = await axios.post(BASE_URL + '/file', formData, config);
+      console.log('image upload result is ', res);
+      return res.data;
+    } catch (err) {
+      console.error('err in axios => ', err);
+      throw err;
+    }
+  };
+
+  const uploadImage = async (file: File) => {
+    setLoading(true);
+    setProgress(0);
+
+    if (!file) return;
+
+    try {
+      const { data } = await uploadMedia(file, (percent, canceler) => {
+        setProgress(percent);
+      });
+
+      const { url, _id } = data;
+      setFileData({ url, _id });
+    } catch (err) {
+      setError(err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, progress, error, uploadImage, fileData };
+};
+
+export default useUploadImage;
