ce8ca5c283afbf5c9feae178e96c51c50199e54d
devarashs <southernwavesofmadness@protonmail.com>
2024-01-18 10:32:37 +0330

    Refactor authentication form and add form validation

--- front/src/components/components-overview/AuthProtocol/index.tsx
+++ front/src/components/components-overview/AuthProtocol/index.tsx
@@ -1,22 +1,12 @@
-import {
-  Button,
-  FormInput,
-  FormSelect,
-  InputGroup,
-  InputGroupAddon,
-} from 'shards-react';
-
 import { useTranslation } from 'react-i18next';
-
 import { useState } from 'react';
-
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
+import { FormControl, Input, Select } from '@mui/material';
 
 import Captcha from '#c/components/captcha';
-
 import styles from '@/assets/styles/Login.module.css';
-
-import { captchaAction, detectUserState } from './utils';
 
 export interface UserProps {
   authStatus:
@@ -24,12 +14,13 @@ export interface UserProps {
     | 'success'
     | 'login:active'
     | 'signup:active'
-    | 'signuo';
-  captcha: boolean;
-  phoneNumber: string;
-  activationCode: string;
-  firstName: string;
-  lastName: string;
+    | 'signup';
+  captcha?: boolean;
+  countryCode?: string;
+  phoneNumber?: string;
+  activationCode?: string;
+  firstName?: string;
+  lastName?: string;
   authenticatingProtocol: 'otp' | 'password';
 }
 
@@ -37,47 +28,109 @@ export default function AuthPortal() {
   const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
   const [userAuthenticationInfo, setUserAuthenticationInfo] =
-    useState<UserProps>();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+    useState<UserProps>({
+      authStatus: 'detect',
+      authenticatingProtocol: 'otp',
+    });
+  const { register } = useForm(); // Remove unused variables
+
+  async function detectUserState(event) {
+    console.log('event data is : ', event);
+    if (!userAuthenticationInfo.captcha) {
+      toast.error('Wrong Captcha!');
+    } else {
+      if (
+        !userAuthenticationInfo.phoneNumber ||
+        userAuthenticationInfo.phoneNumber.trim() === '' ||
+        userAuthenticationInfo.phoneNumber === '0'
+      ) {
+        toast.error('Phone Number Field is Empty');
+      }
+    }
+  }
+
+  function captchaAction(e: boolean) {
+    if (e) {
+      // Set user captcha pass to true
+      setUserAuthenticationInfo((prevState) => ({
+        ...prevState,
+        captcha: true,
+      }));
+    }
+  }
 
   return loading ? (
     <></>
   ) : (
     <div>
       <form
-        onSubmit={handleSubmit(detectUserState)}
+        onSubmit={(values) => detectUserState(values)}
         className={`${styles.container} form-group ltr`}>
-        <label htmlFor="thepho">{t('phone number')}</label>
+        <FormControl
+          margin="normal"
+          fullWidth
+          className={styles.formPhoneFieldContainer}>
+          <label
+            className="center my-2"
+            style={{ textAlign: 'center' }}
+            htmlFor="countryCode">
+            {t('country-code')}
+          </label>
 
-        <InputGroup className="mb-3">
-          <InputGroupAddon type="prepend">
-            <FormSelect>
-              <option value="98">+98</option>
-            </FormSelect>
-          </InputGroupAddon>
-          <FormInput
-            placeholder="**********"
-            id="thepho"
-            className={'iuygfghuji'}
+          <Select
+            id="countryCode"
+            value={userAuthenticationInfo.countryCode}
+            onChange={(event) =>
+              setUserAuthenticationInfo((prevState) => ({
+                ...prevState,
+                countryCode: event.target.value,
+              }))
+            }
+            required // Add required attribute
+          >
+            {[{ value: '+98', label: 'IR ( +98 )' }].map((country) => (
+              <option
+                key={country.value}
+                value={country.value}
+                style={{ textAlign: 'center' }}>
+                {country.label}
+              </option>
+            ))}
+          </Select>
+          <label
+            className="my-2"
+            style={{ textAlign: 'center' }}
+            htmlFor="phoneNumber">
+            {t('phone number')}
+          </label>
+
+          <Input
             type="tel"
-            dir="ltr"
+            id="phoneNumber"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            {...register('phoneNumber', {
+              required: true,
+              minLength: 10,
+            })}
+            value={userAuthenticationInfo.phoneNumber}
+            onChange={(event) =>
+              setUserAuthenticationInfo((prevState) => ({
+                ...prevState,
+                phoneNumber: event.target.value,
+              }))
+            }
+            required // Add required attribute
           />
-        </InputGroup>
+        </FormControl>
+
         <>
           <p>{t('enter captcha')}</p>
           <Captcha onActionSubmit={captchaAction} />
         </>
-        <Button
-          block
-          // type="submit"
-          className="center"
-          onClick={(value) => console.log(value)}>
+        <button disabled={loading} type="submit" className={styles.button}>
           {t('get enter code')}
-        </Button>
+        </button>
       </form>
     </div>
   );
