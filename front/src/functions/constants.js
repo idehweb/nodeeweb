60572b93f49a57bfcf112bdca2b2c825893b7d9a
Erfan <erfanmahvash12345@gmail.com>
2024-01-30 13:11:21 +0330

    fix: circular dependencies

---
+++ front/src/functions/constants.js
@@ -0,0 +1,7 @@
+export const ServerUrl = process.env.REACT_APP_SERVER_URL;
+export const MainUrl = ServerUrl;
+export const ApiUrl = process.env.REACT_APP_API_URL;
+export const AdminRoute = ServerUrl + '/admin';
+export const InstanceManagerUrl = 'https://instancemanager.nodeeweb.com/api/v1';
+export const THEME_URL = ApiUrl + '/theme';
+export const CONFIG_URL = ApiUrl + '/config/website';
