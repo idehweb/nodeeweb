3ff46f15162ff688694114fa917bfd1b4f94c2fa
Erfan <erfanmahvash12345@gmail.com>
2024-02-05 14:24:09 +0330

    fix: base url for order

--- shop/constants/String.ts
+++ shop/constants/String.ts
@@ -4,7 +4,7 @@ export const DEFAULT_SHOP_INACTIVE_MSG =
 export const DEFAULT_ENTRY_SUBMIT_MSG =
   'thanks for your message! your code: %TRACKING_CODE%';
 
-export const DEFAULT_APPROACH_TRANSACTION_EXP_MSG = `ما تنها مدت کوتاهی می‌توانیم سفارش‌تان را نگه ‌داریم و در صورت عدم پرداخت، ناچار به لغو آن هستیم. برای نهایی کردن، از بخش «پیگیری سفارش‌ها» مبلغ آن را پرداخت‌ کنید. شماره سفارش: %ORDER_ID% %BASE_URL%`;
+export const DEFAULT_APPROACH_TRANSACTION_EXP_MSG = `ما تنها مدت کوتاهی می‌توانیم سفارش‌تان را نگه ‌داریم و در صورت عدم پرداخت، ناچار به لغو آن هستیم. برای نهایی کردن، از بخش «پیگیری سفارش‌ها» مبلغ آن را پرداخت‌ کنید. شماره سفارش: %ORDER_ID% %HOST%/profile`;
 
 export const DEFAULT_REGISTER_MSG = 'welcome!';
 export const DEFAULT_CANCEL_ORDER_MSG = 'order $ORDER_ID$ has been cancel';
