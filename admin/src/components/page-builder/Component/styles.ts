3f88f8fcc7e2a373bc0756cda148553347ab039c
Ali Davoudian <ali.davoudiann@gmail.com>
2024-02-19 10:05:36 +0330

    done

--- admin/src/components/page-builder/Component/styles.ts
+++ admin/src/components/page-builder/Component/styles.ts
@@ -14,4 +14,15 @@ export default makeStyles(() => ({
     textAlign: 'center',
     fontSize: 10,
   },
+  title: {
+    textAlign: 'center',
+    fontSize: '1em',
+    fontWeight: 'bold',
+    color: '#fff',
+    backgroundColor: '#232F3E',
+    width: 'auto',
+    padding: '5px',
+    boxShadow:
+      '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  },
 }));
